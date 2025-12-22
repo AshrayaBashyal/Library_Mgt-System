cee353795e9cf829d388ae9304016dcf91995373
Ashraya Bashyal <ashrayabashyal1@gmail.com>
2025-12-22 20:28:52 +0545

    fixing app's views

--- library_app/views.py
+++ library_app/views.py
@@ -1,7 +1,6 @@
 from rest_framework import generics
 from .models import Book, Member, BorrowRecord
 from .serializers import BookSerializer, MemberSerializer, BorrowRecordSerializer
-# Create your views here.
 
 class BookListCreateView(generics.ListCreateAPIView):
     queryset = Book.objects.all()
@@ -15,7 +14,7 @@ class MemberListCreateView(generics.ListCreateAPIView):
     queryset = Member.objects.all()
     serializer_class = MemberSerializer
 
-class MemberRetrieveUpdateDestroyVIew(generics.RetrieveUpdateDestroyAPIView)
+class MemberRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Member.objects.all()
     serializer_class = MemberSerializer
 
