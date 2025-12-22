e37e8371218d149c00a998cb0b7d547bb0da9374
Ashraya Bashyal <ashrayabashyal1@gmail.com>
2025-12-22 20:33:51 +0545

    fixing small bug in app's urls

--- library_app/urls.py
+++ library_app/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     path('books/<int:pk>/', BookRetrieveUpdateDestroyView.as_view()),
 
     path('members/', MemberListCreateView.as_view()),
-    path('members/<int:pk>/', MemberRetrieveUpdateDestroyVIew.as_view()),
+    path('members/<int:pk>/', MemberRetrieveUpdateDestroyView.as_view()),
 
     path('borrows/', BorrowRecordListCreateView.as_view()),
     path('borrows/<int:pk>/', BorrowRecordRetrieveUpdateDestroyView.as_view()),
