19efe13a3938f4ef54996c73235de2f31cf36c38
Ashraya Bashyal <ashrayabashyal1@gmail.com>
2025-12-22 20:23:44 +0545

    creating and setting up apps url

---
+++ library_app/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from .views import *
+
+urlpatterns = [
+    path('books/', BookListCreateView.as_view()),
+    path('books/<int:pk>/', BookRetrieveUpdateDestroyView.as_view()),
+
+    path('members/', MemberListCreateView.as_view()),
+    path('members/<int:pk>/', MemberRetrieveUpdateDestroyVIew.as_view()),
+
+    path('borrows/', BorrowRecordListCreateView.as_view()),
+    path('borrows/<int:pk>/', BorrowRecordRetrieveUpdateDestroyView.as_view()),
+]
